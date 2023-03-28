8b7d614ed38a7a7538f0c4fc6d9f1c79d0aa4376
Oumar <baarryoumar@gmail.com>
2023-03-28 17:14:37 -0400

    question 5 done

--- Midterm Q5.py
+++ Midterm Q5.py
@@ -1,1 +1,9 @@
-print('Hey')
+def question5answer(s):
+  vowels = ['a', 'o', 'i', 'u', 'e']
+  for i in range(0, len(vowels)):
+    s = s.replace(vowels[i], '')
+    s = s.replace(vowels[i].capitalize(), '')
+  return s
+
+s1 = "OIKOw CwiOImWE ooioIQuCdcMDsKmslD kOKPKp"
+print(question5answer(s1))
