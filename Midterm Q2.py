1e8eb1819747c135897f0563cf1bf2f38a4d1961
Oumar <baarryoumar@gmail.com>
2023-03-28 20:59:43 -0400

    question 2 done and corrected

--- Midterm Q2.py
+++ Midterm Q2.py
@@ -12,28 +12,54 @@
 # There is at least one word in s.
 # s does not contain leading or trailing spaces.
 # All the words in s are guaranteed to be separated by a single space.
+
+# def question2answer(s):
+#   if len(s) <= 2 :
+#     return s[::-1] 
+
+#   else :
+#     newStr = "".join(s)
+#     space = " "
+#     reversedStr = newStr.split()[::-1]
+#     new_arr = []
+#     for i in range(len(reversedStr)):
+#       if (reversedStr[i].isdigit()):
+#         new_arr.append(reversedStr[i])
+#       else:
+#         for j in range(len(reversedStr[i])):
+#           word = reversedStr[i]
+#           new_arr.append(word[j])
+#       if i < len(reversedStr) - 1:
+#         new_arr.append(space)
+
+#   return new_arr
+
 def question2answer(s):
+  stack = []
+  result = []
+  final = []
+  newStr = "".join(s)
+  result = newStr.split()
+
   if len(s) <= 2 :
-    return s[::-1]
+    return s[::-1] 
+    
+  for i in range(len(result)):
+    stack.append(result[i])
+  
+  for i in range(len(result)):
+    word = stack.pop()
+    if (word.isdigit()):
+      final.append(word)
+    else:
+      for j in range(len(word)):
+        final.append(word[j])
+    final.append(" ")
 
-  else :
-    newStr = "".join(s)
-    space = " "
-    reversedStr = newStr.split()[::-1]
-    new_arr = []
-    for i in range(len(reversedStr)):
-      if (reversedStr[i].isdigit()):
-        new_arr.append(reversedStr[i])
-      else:
-        for j in range(len(reversedStr[i])):
-          word = reversedStr[i]
-          new_arr.append(word[j])
-      if i < len(reversedStr) - 1:
-        new_arr.append(space)
+  final.pop()
+  return final
 
-  return new_arr
+s= ["t"," "]
 
-
-s1 = s2 = ["t"," "]
-print(question2answer(s1))
+print(question2answer(s))
 
